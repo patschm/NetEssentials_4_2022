1e8fc3268026aa8244137eb3e92faad86b5ca587
Patrick <pschmidt@pasit.nl>
2022-04-21 11:36:18 +0200

    Day1

---
+++ Exercises/Tafels/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO.Compression;
+
+//SchrijfTafels();
+LeestTafels();
+
+void LeestTafels()
+{
+    FileStream fs = File.OpenRead(@"D:\Netessentials_4_2022\Tmp\tafels.zip");
+    DeflateStream zip = new DeflateStream(fs, CompressionMode.Decompress);
+    StreamReader reader = new StreamReader(zip);
+    string? line = null;
+    while((line = reader.ReadLine()) != null)
+    {
+        System.Console.WriteLine(line);
+    }
+}
+
+void SchrijfTafels()
+{
+    FileStream fs = File.OpenWrite(@"D:\Netessentials_4_2022\Tmp\tafels.zip");
+    DeflateStream zip = new DeflateStream(fs, CompressionMode.Compress);
+    StreamWriter writer = new StreamWriter(zip);
+    
+    Console.SetOut(writer);
+    for(int tafel = 1; tafel <=10; tafel++)
+    {
+        Console.WriteLine($"De tafel van {tafel}");
+        for(int teller = 1; teller <= 10; teller++)
+        {
+            Console.WriteLine($"{teller} x {tafel} = {teller * tafel}");
+        }
+        Console.WriteLine();
+    }
+    writer.Flush();
+    writer.Close();
+}
